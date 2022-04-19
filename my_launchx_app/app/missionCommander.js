f30d09c84cba5a13d6992714d3149ca14bc28296
Jose Luis <launchx06519@innovaccion.mx>
2022-04-19 16:43:49 -0500

    Agregando clase, archivo index.js y jest

---
+++ my_launchx_app/app/missionCommander.js
@@ -0,0 +1,7 @@
+class MissionCommander{
+    constructor(name){
+        this.name = name
+    }
+}
+//Nos permite exportar nuestra clase
+module.exports = MissionCommander
---
+++ my_launchx_app/index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const Jose = new MissionCommander("Jose Luis")
+const Ana = new MissionCommander("Ana")
+const Luis  = new MissionCommander("Luis")
+console.log(Jose.name)
+console.log(Ana.name)
+console.log(Luis.name) 
