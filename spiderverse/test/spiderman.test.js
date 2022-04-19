de1490b2d4b5093bec241ebd2892e3cb2d0e1007
Jose Luis <launchx06519@innovaccion.mx>
2022-04-19 18:25:01 -0500

    Guardando directorio spiderverse, app y test

--- spiderverse/test/spiderman.test.js
+++ spiderverse/test/spiderman.test.js
@@ -1,6 +1,16 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require('./../app/spiderman.js')
+describe("Unit Tests for Spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+        //Aqui escribimos el código que queremos usal tal cual
+        //quiero poder instancian un objeto Spiderman con esta infrmacion
+        const andrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield", 2, "Sony")
+        const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+        //Validamos que este código funcione de la forma esperada
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony")
+        expect(tomHolland.getInfo).toBe(`Hey, I'm Tom Holland from Marvel studio`)
     });
   })
