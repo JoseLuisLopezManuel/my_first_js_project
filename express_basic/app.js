7fb426a269ba909a8569dbd700b96bb4af1c69e4
Jose Luis <launchx06519@innovaccion.mx>
2022-04-22 18:05:07 -0500

    2. Agregando una nueva ruta en app.js

--- express_basic/app.js
+++ express_basic/app.js
@@ -9,6 +9,10 @@ const port = 3000
 app.get('/',(req, res) => {
     res.send('Hello World!')
 })
+//localhost:3000/launchx
+app.get('/launchx',(req, res) => {
+    res.send('Bienvenidos a launchX')
+})
 
 //Con esto inicializamos esta app
 app.listen(port, () =>{
