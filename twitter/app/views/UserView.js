538beb98c00fd0d73869cc168632403701c4775f
Jose Luis <launchx06519@innovaccion.mx>
2022-04-22 15:40:16 -0500

    13. Agregando requerimiento 2 de Vistas con su prueba unitaria

--- twitter/app/views/UserView.js
+++ twitter/app/views/UserView.js
@@ -4,6 +4,9 @@ class UserView{
         if(payload == null){
             return {error : "El payload no existe"}
         }
+        else if(payload.username === null || payload.name === null || payload.id === null){
+            return {error : "Necesitan tener un valor válido"}
+        }
     }
 }
 module.exports = UserView
--- twitter/test/views/UserView.test.js
+++ twitter/test/views/UserView.test.js
@@ -7,4 +7,10 @@ describe("Tests for UserView", () => {
         //https://jestjs.io/docs/using-matchers#strings
         expect(result.error).toMatch(/payload no existe/)
     })
+    test("Return an error object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {username:null , name: 12,  id: "id"}
+        const result = UserView.createUser(payload)
+        //https://jestjs.io/docs/using-matchers#strings
+        expect(result.error).toMatch(/Necesitan tener un valor válido/)
+    })
 })
