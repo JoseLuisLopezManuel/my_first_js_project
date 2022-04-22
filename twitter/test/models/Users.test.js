3a363a31927f3433543575f628363846623a540e
Jose Luis <launchx06519@innovaccion.mx>
2022-04-21 20:02:53 -0500

    5. Haciendo mi segunda prueba unitaria

--- twitter/test/models/Users.test.js
+++ twitter/test/models/Users.test.js
@@ -3,7 +3,7 @@ const User = require('./../../app/models/Users.js')
 describe("Unit test for Users class", () => {
     test('Create an Users object', () => {
         //Se invoca el código que se va a usar en el app
-        const User1 = new User(1,"Jose Luis", "Jose", "Bio", "DateCreated", "LastUpdate")
+        const User1 = new User(1,"Jose Luis", "Jose", "Bio")
         //Aqui validas los resultados de ese código
         //Esta es una comparacion que va a igualar el valor de la izquiere con el valor de la derecha (valor esperado)
         expect(User1.id).toBe(1)
@@ -12,6 +12,20 @@ describe("Unit test for Users class", () => {
         expect(User1.bio).toBe("Bio")
         expect(User1.dateCreated).not.toBeUndefined()
         expect(User1.LastUptade).not.toBeUndefined()
+    
+    })
+})
+
+describe("Unit test for Users class", () => {
+    test('Create an Users object', () => {
+        //Se invoca el código que se va a usar en el app
+        const User2 = new User(1,"Jose Luis", "Jose", "Bio")
+        //Aqui validas los resultados de ese código
+        //Esta es una comparacion que va a igualar el valor de la izquiere con el valor de la derecha (valor esperado)
+        expect(User2.getUsername).toBe("Jose Luis")
+        expect(User2.getBio).toBe("Bio")
+        expect(User2.getDateCreated).not.toBeUndefined()
+        expect(User2.getLastUptade).not.toBeUndefined()
     
     })
 })
