9388cdca0ba24440cd75af1b003abdce9a5d74ca
Jose Luis <launchx06519@innovaccion.mx>
2022-04-22 20:30:38 -0500

    8. Agregar DELETE ENDPOINT en app.js

--- express_api/app.js
+++ express_api/app.js
@@ -50,6 +50,14 @@ app.put('/v1/explorers/:id', (req,res) =>{
     const requestBody = req.body //Párametros de un cliente
     res.status(200).json({message:"Actualizado exitosamente"})
 })
+
+app.delete('/v1/explorers/:id',(req, res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`)
+    //agregar la logica para eliminar
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body //Parámetros de un cliente
+    res.status(200).json({message:"Deleted"})
+})
 //Con esto inicializamos esta app
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`)
